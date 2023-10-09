ee7567871b63a47cbe7a6809a236dda2e972a528
vignesh-vuram <vigneshs@vuram.com>
2023-10-09 22:13:37 +0530

    initial changes

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from '../Loader';
+
+const Ball = (props) => {
+  // const [decal] = useTexture([props.imgUrl]);
+  return <div>Ball</div>;
+};
+
+export default Ball;
